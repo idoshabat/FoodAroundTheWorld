1c9b044cce1306faf89f5098eb55d7f3fab4d273
ido shabat <idoshabat99@gmail.com>
2023-10-29 14:17:40 +0200

    activate server

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const User = require('./models/user');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/foodAroundTheWorld')
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "connection error"));
+db.once("open" , ()=>{
+    console.log('Database connected');
+})
+
+const app = express();
+
+app.set('views' , path.join(__dirname, 'views'));
+app.set('view engine' , 'ejs');
+app.use(express.static('public'));
+
+let logged_in=false;
+
+app.get('/' , (req,res) =>{
+    res.render('home');
+})
+
+// app.get('/myArea' , (req,res) => {
+//     if(logged_in){
+
+//     }
+//     else{
+
+//     }
+// })
+
+app.get('/:page' , (req,res) => {
+    const {page} = req.params;
+    res.render(`${page}`)
+})
+
+app.get('/food/:foodType' , (req,res) => {
+    const {foodType} = req.params;
+    res.render('foodType' , {foodType})
+})
+
+app.listen(3000, async ()=>{
+    console.log('Yeaaaa');
+})
