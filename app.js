85e69725d11b9e30f95ba00fa11b28d5b5baff6f
ido shabat <idoshabat99@gmail.com>
2023-10-29 14:53:36 +0200

    adding navbar

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const User = require('./models/user');
+const ejsMate = require('ejs-mate');
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/foodAroundTheWorld')
@@ -17,6 +18,7 @@ const app = express();
 app.set('views' , path.join(__dirname, 'views'));
 app.set('view engine' , 'ejs');
 app.use(express.static('public'));
+app.engine('ejs',ejsMate);
 
 let logged_in=false;
 
